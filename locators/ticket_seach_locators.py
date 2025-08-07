a3fdb2fba685ab97d5c9185ef969fdb6b15c8b38
MaksimFilatov <Fillife.one@ya.ru>
2025-08-07 17:47:37 +0300

    Добавлены локаторы: билеты на автобусы, поезда, самолеты, откуда, куда, выпадающее меню сортировки, сортировка по цене

--- locators/ticket_seach_locators.py
+++ locators/ticket_seach_locators.py
@@ -2,6 +2,17 @@ from selenium.webdriver.common.by import By
 
 
 class TicketSearchLocators:
-    INPUT_FROM = (By.XPATH, "//span[@data-ti='input-label'][text()='Откуда']")
-    INPUT_TO = (By.XPATH, "//span[@data-ti='input-label'][text()='Куда']")
+
+    # ---Выбор катигории---
+    BUSS_TICKET = (By.XPATH, "//button[.//span[text()='Автобусы']]")
+    TRAIN_TICKET = (By.XPATH, "//button[.//span[text()='Ж/д билеты']]")
+    AVIA_TICKET = (By.XPATH, "//button[.//span[text()='Авиабилеты']]")
+
+    # ---Поиск билетов---
+    INPUT_FROM = (By.XPATH, "//input[@data-ti='input'][following-sibling::span[text()='Откуда']]")
+    INPUT_TO = (By.XPATH, "//input[@data-ti='input'][following-sibling::span[text()='Куда']]")
     SEARCH_BUTTON = (By.CSS_SELECTOR, "button[data-ti='submit-button']")
+
+    # ---Сортировки---
+    SORT_BUTTON = (By.XPATH, "//button[.//span[@data-ti='name_option'][text()='Сортировка']]")
+    SORT_FROM_PRICE = (By.XPATH, "//div[contains(@class, 'o-select-selected') and contains(., 'Стоимость')]")
