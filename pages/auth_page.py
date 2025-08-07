6daa25d3a65f7fa419a0131d0c29419e3e0440c7
MaksimFilatov <Fillife.one@ya.ru>
2025-08-07 21:26:28 +0300

    Добавлены методы разлогина и проверки разлогина

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -11,38 +11,47 @@ class AuthorizationPage:
         self.driver.get("https://www.tutu.ru/")
 
     def open_authorization_page(self):
-        enter_button = self.driver.find_element(locator.BTN_ENTER)
+        enter_button = self.driver.find_element(*locator.BTN_ENTER)
         enter_button.click()
 
     def fill_email(self, email):
-        email_box = self.driver.find_element(locator.INPUT_EMAIL)
+        email_box = self.driver.find_element(*locator.INPUT_EMAIL)
         email_box.send_keys(email)
 
     def login_with_password(self):
-        password_button = self.driver.find_element(locator.BTN_SWITCH_TO_PASSWORD)
+        password_button = self.driver.find_element(*locator.BTN_SWITCH_TO_PASSWORD)
         password_button.click()
 
     def fill_password(self, password):
-        password_box = self.driver.find_element(locator.INPUT_PASSWORD)
+        password_box = self.driver.find_element(*locator.INPUT_PASSWORD)
         password_box.send_keys(password)
 
     def fill_wrong_password(self, wr_pas):
-        password_box = self.driver.find_element(locator.INPUT_PASSWORD)
+        password_box = self.driver.find_element(*locator.INPUT_PASSWORD)
         password_box.send_keys(wr_pas)
 
     def submit_authorization(self):
-        submit_authorization = self.driver.find_element(locator.BTN_SUBMIT_LOGIN)
+        submit_authorization = self.driver.find_element(*locator.BTN_SUBMIT_LOGIN)
         submit_authorization.click()
 
     def open_menu(self):
-        open_menu = WebDriverWait(self.driver, 10).until(
+        open_menu = self.wait.until(
             EC.element_to_be_clickable(locator.BTN_MENU))
         open_menu.click()
 
     def check_successful_authorization(self):
-        check_successful_authorization = self.driver.find_element(locator.AUTH_SUCCESS)
+        check_successful_authorization = self.wait.until(
+            EC.visibility_of_element_located(locator.AUTH_SUCCESS))
         assert check_successful_authorization.is_displayed()
 
     def check_message_auth_error(self):
-        auth_error = self.driver.find_element(locator.AUTH_ERROR_MESSAGE)
+        auth_error = self.driver.find_element(*locator.AUTH_ERROR_MESSAGE)
         assert auth_error.is_displayed()
+
+    def logout_user(self):
+        logout_user = self.driver.find_element(*locator.LINK_LOGOUT)
+        logout_user.click()
+
+    def check_logout(self):
+        logout = self.driver.find_element(*locator.BTN_ENTER)
+        assert logout.is_displayed()
